3a602c3d429fea43f21bf433a24ec922091595e1
TraistaRafael <traista.rafael@gmail.com>
2021-10-23 19:20:51 +0300

    LogoSmash demo ready - sporadic directx crash.

--- tests/LogoSmash.hx
+++ tests/LogoSmash.hx
@@ -5,106 +5,114 @@ import hl.Bytes;
 
 class LogoSmash extends hxd.App{
 
-	public var space:chipmunk.Native.CpSpace;
-	public var shape:chipmunk.Native.CpShape;
-	public var body:chipmunk.Native.CpBody;
+	static var space:chipmunk.Native.CpSpace;
 
-	var bmp : h2d.Bitmap;
+	var cpShapesList : Array<chipmunk.Native.CpShape> = [];
+	var heapsObjects : Array<h2d.Bitmap> = [];
 
-    static function main() {
-		trace("LogoSmash");
-		new LogoSmash();
-    }
+	var circleRadius : Int = 4;
 
-	public function makeBall(x : Float, y :Float) : chipmunk.Native.CpShape{
-		body = chipmunk.Native.CpBody.cpBodyNew(1.0, Math.POSITIVE_INFINITY);
+	public function makeBall(x : Float, y : Float, radius : Float) : chipmunk.Native.CpShape{
+		var body = chipmunk.Native.CpBody.cpBodyNew(1.0, Math.POSITIVE_INFINITY);
 
 		var pos = new chipmunk.Native.CpVect();
 		pos.x = x;
 		pos.y = y;
 		body.cpBodySetPosition(pos);
-
+		
 		var cpvzero = new chipmunk.Native.CpVect();
 		cpvzero.x = 0;
 		cpvzero.y = 0;
-		var shape = chipmunk.Native.CpCircleShape.cpCircleShapeNew(body, 0.95, cpvzero);
+		var shape = chipmunk.Native.CpCircleShape.cpCircleShapeNew(body, radius, cpvzero);
 		shape.cpShapeSetElasticity(0.0);
 		shape.cpShapeSetFriction(0.0);
 		
+		space.cpSpaceAddBody(body);
+		space.cpSpaceAddShape(shape);
+
 		return shape;
 	}
 
+	function syncHeapsAndPhysics() {
+		// Sync chipmunk physical objects with heaps GUI
+		for (i in 0...cpShapesList.length){
+			var cpShape = cpShapesList[i];
+			var cpBody = cpShape.cpShapeGetBody();
+			var heapsObj = heapsObjects[i];
+
+			var pos = cpBody.cpBodyGetPosition();
+			heapsObj.x = pos.x;
+			heapsObj.y = pos.y;
+		}
+	}
+
 	override function init() {
-
 		super.init();
-		
-		@:privateAccess hxd.Window.getInstance().window.title = "LogoSmash Example";
 
-        var scene2d = new h2d.Scene();
+		hxd.Res.initEmbed();
+
+		// Init GUI
+		@:privateAccess hxd.Window.getInstance().window.title = "LogoSmash Example";   
+		var scene2d = new h2d.Scene();
         setScene(scene2d);
 
-		var tile = h2d.Tile.fromColor(0xFF0000, 5, 5);
-		bmp = new h2d.Bitmap(tile, s2d);
-		bmp.x = s2d.width * 0.5; 
-		bmp.y = s2d.height * 0.5;
-
-		// var shapes = [bullet.Shape.createSphere(0.5), bullet.Shape.createBox(1,1,1)];
-		// for( i in 0...100 ) {
-		// 	var id = Std.random(shapes.length);
-		// 	var b = new bullet.Body(shapes[id], 0.5, world);
-		// 	var m = b.initObject();
-		// 	m.x = Math.random() * 10;
-		// 	m.y = Math.random() * 10;
-		// 	m.z = 10 + Math.random() * 10;
-		// 	b.loadPosFromObject();
-
-		// 	var mt = new h3d.Matrix();
-		// 	mt.identity();
-		// 	mt.colorHue(Math.random() * Math.PI * 2);
-		// 	m.material.color.set(0.5, 0.3, 0);
-		// 	m.material.color.transform(mt);
-		// }
-
-		space = chipmunk.Native.CpSpace.cpSpaceNew();
-		space.cpSpaceSetIterations(1);
-		space.cpSpaceUseSpatialHash(2.0, 10000);
-
-		var bodyCount:Int = 0;
-
-		shape = makeBall(bmp.x, bmp.y);
-		space.cpSpaceAddBody(shape.cpShapeGetBody());
-		space.cpSpaceAddShape(shape);
 		
+		
+
+		//var tile = h2d.Tile.fromColor(0xFF0000, circleRadius * 2, circleRadius * 2);
+		var tile = hxd.Res.red_circle.toTile();
+
+		var offsetx = 400;
+		var offsety = 300;
+		var margin = 4;
+
+		for(i in 0...10){
+			for(j in 0...10){	
+			// Compute position for the next object
+			var x = offsetx + i * (circleRadius * 2 + margin); 
+			var y = offsety + j * (circleRadius * 2 + margin); 
+			
+			// Init chipmunk object for physics
+			var shape = makeBall(x, y, circleRadius);
+			cpShapesList.push(shape);
+
+			// Init heaps object for GUI
+			var bmp = new h2d.Bitmap(tile, s2d);
+			bmp.x = x; 
+			bmp.y = y;
+			heapsObjects.push(bmp);
+			}
+		}
+
+		// Init chipmunk object for phisics moving object
+		var shape = makeBall(20, 320, circleRadius);
+	
+		// Set velocity to the chipmunk object
 		var velocity = new chipmunk.Native.CpVect();
-		velocity.x = 100;
+		velocity.x = 200;
 		velocity.y = 0;
-		body.cpBodySetVelocity(velocity);
-
-		// bodyCount++;
-
-		// for(i in 0...10){
-		// 	for(j in 0...10){
-		// 		// if(!get_pixel(x, y)) continue;
-				
-		// 		// cpFloat x_jitter = 0.05*frand();
-		// 		// cpFloat y_jitter = 0.05*frand();
-				
-		// 		var shape = makeBall(0, 0);
-		// 		space.cpSpaceAddBody(shape.cpShapeGetBody());
-		// 		space.cpSpaceAddShape(shape);
-				
-		// 		bodyCount++;
-		// 	}
-		// }
+		shape.cpShapeGetBody().cpBodySetVelocity(velocity);
+		cpShapesList.push(shape);
+		
+		// Init chipmunk object for GUI moving object
+		var greenTile = hxd.Res.green_circle.toTile();
+		var bmp = new h2d.Bitmap(greenTile, s2d);
+		bmp.x = 20; 
+		bmp.y = 320;
+		heapsObjects.push(bmp);
 	}
 
 	override function update(dt:Float) {
 		space.cpSpaceStep(dt);
-		bmp.x += 1;
-		var pos = body.cpBodyGetPosition();
-		bmp.x = pos.x;
-		bmp.y = pos.y;
-		//trace('shape pos: ${pos.x}, ${pos.y}');
+		syncHeapsAndPhysics();
 	}
 
+	static function main() {
+		// Init Chipmunk physics space
+		space = chipmunk.Native.CpSpace.cpSpaceNew();
+		space.cpSpaceSetIterations(1);
+		//space.cpSpaceUseSpatialHash(2.0, 10000);
+
+		new LogoSmash();
+    }
 }
